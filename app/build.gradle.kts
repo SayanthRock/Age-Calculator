099ed7f57602a747f516bb286958d6f36ca7d5c0
Sayanth Rock <202829406+SayanthRock@users.noreply.github.com>
2026-06-08 22:19:00 +0530

    AgeCalculator

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,9 +48,16 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
   }
+
+  tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+      jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+  }
+
   buildFeatures {
     compose = true
     buildConfig = true
